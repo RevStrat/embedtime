bb61cbe753358fce19770afed3d27c3dfa7ce8ac
Brett Whitaker <brett@civilized.life>
2018-07-19 11:36:54 -0300

    Update path to css requirement

--- src/EmbedTimeControllerExtension.php
+++ src/EmbedTimeControllerExtension.php
@@ -7,6 +7,6 @@ use SilverStripe\View\Requirements;
 
 class EmbedTimeControllerExtension extends DataExtension {
     public function onAfterInit() {
-        Requirements::css(EMBEDTIME_DIR . '/css/embed.css');
+        Requirements::css('revstrat/embedtime:css/embed.css');
     }
 }
